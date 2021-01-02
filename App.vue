28984b7a41c0a0eb215d0a44c840346e42d4881d
KT <tangkaichuan@qq.com>
2021-01-02 20:50:27 +0800

    perf: refine code

--- App.vue
+++ App.vue
@@ -1,18 +1,14 @@
 <script>
 export default {
   globalData: {
-    _i18n: '',
-    $t: ''
+    _i18n: {},
+    $t: {}
   },
   onLaunch: function() {
     console.log('App Launch')
-
-    const context = this
-    // 这是原型链上的 $i18n
+	
     this.globalData._i18n = this.$i18n
-    this.globalData.$t = function(str) {
-      return context.$t(str)
-    }
+    this.globalData.$t = str => this.$t(str)
   },
   onShow: function() {
     console.log('App Show')
--- lang/index.js
+++ lang/index.js
@@ -1,26 +1,26 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import langEn from './en'
-import langZhCN from './zh_CN'
+import langZhCN from './zh-CN'
 
 Vue.use(VueI18n)
 
-const system_info = uni.getStorageSync('system_info')
-if (!system_info) {
+let currentLang = uni.getStorageSync('CURRENT_LANG')
+if (!currentLang) {
 	// 获取设备信息
 	uni.getSystemInfo({
 		success: function (res) {
-			uni.setStorageSync('system_info', res);
+			uni.setStorageSync('CURRENT_LANG', res.language)
+			currentLang = res.language
 		}
 	})
 }
-const currentLang = system_info.language === 'en' ? 'en' : 'zh_CN'
 const i18n = new VueI18n({
 	// 默认选择的语言
-	locale: currentLang || 'zh_CN',
+	locale: currentLang,
 	messages: {
 		'en': langEn,
-		'zh_CN': langZhCN
+		'zh-CN': langZhCN
 	}
 })
 export default i18n
